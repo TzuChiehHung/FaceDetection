b9c5fae1d0d81d3eb21cea39fcdb13910372a23f
tc.hung <hungtc@solab.me.ntu.edu.tw>
2018-08-29 10:22:59 +0800

    Add simple https server

---
+++ server.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+import BaseHTTPServer, SimpleHTTPServer
+import ssl
+
+httpd = BaseHTTPServer.HTTPServer(('192.168.1.47', 4443), SimpleHTTPServer.SimpleHTTPRequestHandler)
+httpd.socket = ssl.wrap_socket(httpd.socket, certfile='./server.pem', server_side=True)
+
+# try:
+httpd.serve_forever()
+
+
